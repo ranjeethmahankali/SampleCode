49f4a1bf61d9c8b408adaf84237b8da37450bbeb
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2018-06-02 19:05:58 -0700

    annotations

--- ConvexHull/ConvexHull.py
+++ ConvexHull/ConvexHull.py
@@ -1,6 +1,13 @@
+"""
+Implementation of the convex hull algorithm -
+calculates the convex hull (polygon) for a given collection
+of points in a plane.
+Related Video: https://youtu.be/4y6LORX2Ysg
+"""
 import rhinoscriptsyntax as rs
 import time
 
+# returns true if the turn represented by the list of points is a right turn
 def IsValidTurn(pts):
 	assert len(pts) == 3
 	v1 = rs.VectorSubtract(pts[1], pts[0])
@@ -8,12 +15,15 @@ def IsValidTurn(pts):
 	cross = rs.VectorCrossProduct(v2, v1)
 	return cross[2] > 0
 
+# adds the point to the polygon (vertex list) while making sure all the turns
+# are right turns
 def SafeExtend(polygon, newPt):
 	polygon.append(newPt)
 	while len(polygon) > 2 and (not IsValidTurn(polygon[-3:])):
 		del polygon[-2]
 	return polygon
 
+# the main algorithm
 def ConvexHull(pointIds):
 	points = rs.coerce3dpointlist(pointIds)
 	points = sorted(points, key=lambda p: p[0])
@@ -32,6 +42,9 @@ def ConvexHull(pointIds):
 	return hull
 		
 if __name__ == "__main__":
+        # get the points from the user
 	pts = rs.GetObjects("Select points", filter=rs.filter.point)
+	# compute the convex hull
 	hull = ConvexHull(pts)
+	# visualize the hull as a polyline
 	rs.AddPolyline(hull)
